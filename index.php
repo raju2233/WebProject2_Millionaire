107db0886fb2a2e9fead9312a0709cfd8f206a17
Ramya Sree <ramyasree@Ramyas-MacBook-Pro.local>
2024-11-10 14:45:44 -0500

    game changes

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Treasure Trivia</title>
+    <link rel="stylesheet" href="index_style.css">
+    <link rel="icon" type="image/x-icon" href="images/back.gif">
+    <link rel="stylesheet" type="text/css" href="path/to/your/Algerian-font.css">
+    <style>
+        body {
+            background-image: url('images/home_logo.gif');
+            background-size: cover;
+            background-repeat: no-repeat;    
+        }
+        {
+            text-decoration: none;
+            color: white;
+            border: 2px solid white;
+            padding: 5px;
+            border-radius: 50px;
+            font-size: xx-large;
+            background-color: darkblue;
+            margin-left: 32%;
+            position: absolute; 
+            bottom: 227px; 
+        }
+        img{
+            float:right;
+        }
+    </style>
+</head>
+<body>
+    <audio controls loop autoplay src="index.mp3" style="display:none"></audio>
+    <a href="login.php"><img src="images/login_button.png" alt="login" width=80px height=80px></a>
+</body>
+</html>
